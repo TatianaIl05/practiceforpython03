017e9eb823d9eb5d3f972b7990ce0e7c1a976d1a
Татьяна Ильиных <t.ilinykh1@g.nsu.ru>
2024-03-02 11:20:59 +0700

    Update task_11-lengths.py

--- task_11-lengths.py
+++ task_11-lengths.py
@@ -1,8 +1,16 @@
 import math
 
 a, b, c = map(int, input('Введите длины сторон треугольника: ').split())
-if a + b > c and a+c > b and b+c > a:
-    alpha = math.degrees(math.acos((b**2 + c**2 - a**2) / (2*b*c)))
-    betta = math.degrees(math.acos((a ** 2 + c ** 2 - b ** 2) / 2 * a * c))
-    gamma = math.degrees(int(math.acos((a**2+b**2-c**2)/2*a*b)))
-    print(f'Угол напротив a = {alpha}, угол напротив b = {betta}, угол напротив c = {gamma}.')
+
+if a < b+c and b < a+c and c < a+b:
+    cos_a = (b**2 + c**2 - a**2) / (2 * b * c)
+    angle_a = math.degrees(math.acos(cos_a))
+    cos_b = (a**2 + c**2 - b**2) / (2 * a * c)
+    angle_b = math.degrees(math.acos(cos_b))
+    cos_c = (b**2 + a**2 - c**2) / (2 * b * a)
+    angle_c = math.degrees(math.acos(cos_c))
+
+    print(f'Угол напротив a = {round(angle_a)}, угол напротив b = {round(angle_b)}, угол напротив c = {round(angle_c)}')
+    
+else:
+    print('Неверные длины')
