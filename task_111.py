6202ec217f27831e062ee350e8886d53ec6de330
Татьяна Ильиных <t.ilinykh1@g.nsu.ru>
2024-03-02 12:03:24 +0700

    Update and rename task_11-lengths.py to task_111.py

--- task_111.py
+++ task_111.py
@@ -7,7 +7,7 @@ if a < b+c and b < a+c and c < a+b:
     angle_a = math.degrees(math.acos(cos_a))
     cos_b = (a**2 + c**2 - b**2) / (2 * a * c)
     angle_b = math.degrees(math.acos(cos_b))
-    cos_c = (b**2 + a**2 - c**2) / (2 * b * a)
+    cos_c = (b**2 + a**2 - c**2) / (2 * a * b)
     angle_c = math.degrees(math.acos(cos_c))
 
     print(f'Угол напротив a = {round(angle_a)}, угол напротив b = {round(angle_b)}, угол напротив c = {round(angle_c)}')
